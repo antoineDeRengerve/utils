15ebf376d1b062dbdc0d9ddb41f2d0c394645faf
antoine.derengerve <antoine.derengerve@partnering.fr>
2020-01-09 16:02:54 +0100

    Emacs uses locally installed eslint

--- emacs/init.el
+++ emacs/init.el
@@ -535,3 +535,18 @@
       ad-do-it)
     ad-do-it))
 
+;; searches the current files parent directories for the 
+;; node_modules/.bin/ directory and adds it to the buffer 
+;; local exec-path
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint
+          (and root
+               (expand-file-name "node_modules/.bin/eslint"
+                                 root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
